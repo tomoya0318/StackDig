009fdc431fa6def310977975eb3aeaee4ff4c091
tomoya-n <s266227@wakayama-u.ac.jp>
2025-05-16 18:25:05 +0900

    クエリへのpath情報を追加

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ ROOT = Path(__file__).parents[2]
 DATA = ROOT / "data"
 DB_DIR = ROOT / "db" # データベースのディレクトリ
 DB_PATH = DATA / "database.db" # 保存するためのデータベースのパス
+QUERY_DIR = DB_DIR / "queries" # SQLクエリのディレクトリ
 
 # OSSのデータソース設定
 OPENSTACK_API_URL = "https://review.opendev.org/a"
